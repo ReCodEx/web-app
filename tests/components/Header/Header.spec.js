405b4cb7e4f58fdac0d804436c064af54b684944
Simon Rozsival <simon@rozsival.com>
2016-05-05 19:32:15 +0200

    Skipping non-finished tests

--- tests/components/Header/Header.spec.js
+++ tests/components/Header/Header.spec.js
@@ -3,7 +3,7 @@ import { Header } from 'components/Header/Header';
 import { IndexLink, Link } from 'react-router';
 import { shallow } from 'enzyme';
 
-describe('(Component) Header', () => {
+describe.skip('(Component) Header', () => {
   let _wrapper;
 
   beforeEach(() => {
