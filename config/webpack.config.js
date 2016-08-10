c457784334a16e684f82d6aa24f94286453ccf13
Simon Rozsival <simon@rozsival.com>
2016-08-10 16:24:42 +0200

    Unnecessary webpack loaders removed

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -14,16 +14,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loaders: [ 'babel' ] },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.css$/, loader: 'isomorphic-style!css?modules&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version' },
-      { test: /\.less$/, loader: 'isomorphic-style!css?modules&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!less?outputStyle=expanded&sourceMap=true&sourceMapContents=true' },
-      { test: /\.scss$/, loader: 'isomorphic-style!css?modules&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap=true&sourceMapContents=true' },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
-      { test: /\.(jpg|png|gif)/, loader: 'url-loader?limit=10240' }
+      { test: /\.json$/, loader: 'json-loader' }
     ]
   },
   resolve: {
