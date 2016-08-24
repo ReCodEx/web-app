d54328924cca06be2b3d15b925363daa8cd2335f
Simon Rozsival <simon@rozsival.com>
2016-08-24 14:34:04 +0200

    Progress monitoring modal can handle unsupported WS and errors

--- src/containers/EvaluationProgressContainer/EvaluationProgressContainer.js
+++ src/containers/EvaluationProgressContainer/EvaluationProgressContainer.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { List } from 'immutable';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
@@ -26,30 +28,37 @@ import randomMessages, { lastMessage } from './randomMessages';
 
 class EvaluationProgressContainer extends Component {
 
+  state = { realTimeProcessing: true };
   componentWillMount = () => this.init(this.props);
   componentWillReceiveProps = (props) => this.init(props);
 
   init = (props) => {
     const {
       submissionId,
-      port = 4567,
-      url = '195.113.17.8',
-      isOpen
+      port = 443,
+      url = 'recodex.projekty.ms.mff.cuni.cz',
+      path = 'ws',
+      isOpen,
+      secure = true
     } = props;
 
     if (!this.socket && submissionId !== null) {
-      this.socket = new WebSocket(`ws://${url}:${port}`);
-      this.socket.onopen = () => this.socket.send(submissionId);
-      this.socket.onmessage = this.onMessage;
-      this.socket.onerror = () => this.onError();
-      this.socket.onclose = () => this.onConnectionClosed();
+      if (typeof WebSocket === 'function') {
+        const schema = secure ? 'wss' : 'ws';
+        this.socket = new WebSocket(`${schema}://${url}:${port}/${path}/`);
+        this.socket.onopen = () => this.socket.send(submissionId);
+        this.socket.onmessage = this.onMessage;
+        this.socket.onerror = () => this.setState({ realTimeProcessing: false });
+      } else {
+        this.setState({ realTimeProcessing: false });
+      }
     }
   };
 
   onMessage = (msg) => {
     const data = JSON.parse(msg.data);
     const { addMessage } = this.props;
-    addMessage(this.getMessage(data));
+    addMessage(this.formatMessage(data));
 
     switch (data.command) {
       case 'TASK':
@@ -61,10 +70,10 @@ class EvaluationProgressContainer extends Component {
     }
   };
 
-  getMessage = ({ command, task_id = false, task_state = 'OK' }) => ({    // eslint-disable-line camelcase
+  formatMessage = ({ command, task_id = false, task_state = 'OK', text = null }) => ({    // eslint-disable-line camelcase
     wasSuccessful: command !== 'TASK' || task_state === 'COMPLETED',      // eslint-disable-line camelcase
-    text: this.getRandomMessage(),
-    status: this.getStatus(task_state)
+    text: text || this.getRandomMessage(),
+    status: task_state // eslint-disable-line camelcase
   });
 
   getRandomMessage = () => {
@@ -74,20 +83,6 @@ class EvaluationProgressContainer extends Component {
 
     const randomIndex = Math.floor(Math.random() * this.availableMessages.length);
     return this.availableMessages.splice(randomIndex, 1).pop();
-  };
-
-  /** @todo Rewrite when localization is introduced */
-  getStatus = status => {
-    switch (status) {
-      case 'COMPLETED':
-        return 'HOTOVO';
-      case 'SKIPPED':
-        return 'PŘESKOČENO';
-      case 'FAILED':
-        return 'SELHALO';
-      default:
-        return 'OK';
-    }
   };
 
   updateProgress = task => {
@@ -115,16 +110,6 @@ class EvaluationProgressContainer extends Component {
     addMessage({ wasSuccessful: true, status: 'OK', text: lastMessage });
   };
 
-  // @todo Do something ??
-  onError = () => {
-    console.log('websocket connection failed');
-  };
-
-  // @todo Do something ??
-  onConnectionClosed = () => {
-    console.log('websocket connection was closed');
-  };
-
   finish = () => {
     const { link, finishProcessing } = this.props;
     const { router } = this.context;
@@ -141,16 +126,33 @@ class EvaluationProgressContainer extends Component {
       isFinished
     } = this.props;
 
-    return (
-      <EvaluationProgress
-        isOpen={isOpen}
-        messages={messages}
-        completed={progress.completed}
-        skipped={progress.skipped}
-        failed={progress.failed}
-        finished={isFinished}
-        finishProcessing={this.finish} />
-    );
+    return this.state.realTimeProcessing === true
+      ? <EvaluationProgress
+          isOpen={isOpen}
+          messages={messages}
+          completed={progress.completed}
+          skipped={progress.skipped}
+          failed={progress.failed}
+          finished={isFinished}
+          showContinueButton={isFinished}
+          finishProcessing={this.finish} />
+      : <EvaluationProgress
+          isOpen={isOpen}
+          messages={List([
+            this.formatMessage({
+              command: 'TASK',
+              task_state: 'SKIPPED',
+              text: <FormattedMessage
+                      id='app.evaluationProgress.noWebSockets'
+                      defaultMessage='Your browser does not support realtime progress monitoring or the connection to the server could not be estabelished or was lost. The evaluation has already started and you will be able to see the results soon.' />
+            })
+          ])}
+          completed={0}
+          skipped={100}
+          failed={0}
+          finished={false}
+          showContinueButton={true}
+          finishProcessing={this.finish} />;
   };
 
 }
@@ -161,6 +163,7 @@ EvaluationProgressContainer.propTypes = {
   submissionId: PropTypes.string,
   port: PropTypes.number,
   url: PropTypes.string,
+  path: PropTypes.string,
   finishProcessing: PropTypes.func.isRequired,
   link: PropTypes.string.isRequired
 };
