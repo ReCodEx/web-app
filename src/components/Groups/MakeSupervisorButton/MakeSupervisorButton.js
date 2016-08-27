e6bca4e1cc6e329d2b8aea7feb1b638be5734608
Simon Rozsival <simon@rozsival.com>
2016-08-27 10:30:56 +0200

    Make/remove supervisor button

---
+++ src/components/Groups/MakeSupervisorButton/MakeSupervisorButton.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Button } from 'react-bootstrap';
+import Icon from 'react-fontawesome';
+
+const MakeSupervisorButton = ({ onClick, ...props }) => (
+  <Button {...props} onClick={onClick} bsStyle='success' className='btn-flat'>
+    <Icon name='user-plus' /> <FormattedMessage id='app.groups.makeSupervisorButton' defaultMessage='Make supervisor' />
+  </Button>
+);
+
+MakeSupervisorButton.propTypes = {
+  onClick: PropTypes.func.isRequired
+};
+
+export default MakeSupervisorButton;
