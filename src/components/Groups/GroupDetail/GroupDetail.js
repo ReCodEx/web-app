d80c18e25e32990b6049f56b954f6631d19c1b64
Simon Rozsival <simon@rozsival.com>
2016-09-15 21:29:48 +0200

    Group view update

--- src/components/Groups/GroupDetail/GroupDetail.js
+++ src/components/Groups/GroupDetail/GroupDetail.js
@@ -5,23 +5,27 @@ import { Row, Col, Button } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
 import Box from '../../AdminLTE/Box';
-import LeaveJoinGroupButtonContainer from '../../../containers/LeaveJoinGroupButtonContainer';
 
 const GroupDetail = ({
-  group: { id, description }
+  id,
+  description
 }) => (
-  <div>
-    <Box
-      title={<FormattedMessage id='app.groupDetail.description' defaultMessage='Group description' />}
-      type='primary'
-      collapsable
-      noPadding={false}>
-      <ReactMarkdown source={description} />
-    </Box>
-    <p className='text-center'>
-      <LeaveJoinGroupButtonContainer groupId={id} />
-    </p>
-  </div>
+  <Row>
+    <Col sm={12}>
+      <Box
+        title={<FormattedMessage id='app.groupDetail.description' defaultMessage='Group description' />}
+        type='primary'
+        collapsable
+        noPadding={false}>
+        <ReactMarkdown source={description} />
+      </Box>
+    </Col>
+  </Row>
 );
+
+GroupDetail.propTypes = {
+  id: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
 
 export default GroupDetail;
