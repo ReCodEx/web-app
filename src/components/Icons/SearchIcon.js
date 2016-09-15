5b6d383be466313c681433bef55323167fd9f3c3
Simon Rozsival <simon@rozsival.com>
2016-09-15 21:27:40 +0200

    Search icon

---
+++ src/components/Icons/SearchIcon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Icon from 'react-fontawesome';
+
+const SearchIcon = (props) =>
+  <Icon {...props} name='search' />;
+
+export default SearchIcon;
--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,6 +1,7 @@
 export { default as LoadingIcon } from './LoadingIcon';
 export { default as FailedIcon } from './FailedIcon';
 export { default as WarningIcon } from './WarningIcon';
+export { default as SearchIcon } from './SearchIcon';
 export { default as SendIcon } from './SendIcon';
 export { default as UploadIcon } from './UploadIcon';
 export { default as SuccessIcon } from './SuccessIcon';
