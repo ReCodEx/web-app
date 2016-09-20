014d22a2eeb6874e675499df1fbda916c3516971
Simon Rozsival <simon@rozsival.com>
2016-09-20 12:17:08 +0200

    Resource renderer component

---
+++ src/components/ResourceRenderer/ResourceRenderer.js
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { LoadingIcon, WarningIcon } from '../Icons';
+import { isLoading, isReady, hasFailed, getJsData } from '../../redux/helpers/resourceManager';
+
+const defaultLoading = (
+  <span>
+    <LoadingIcon /> <FormattedMessage id='app.resourceRenderer.loading' defaultMessage='Loading ...' />
+  </span>
+);
+
+const defaultFailed = (
+  <span>
+    <WarningIcon /> <FormattedMessage id='app.resourceRenderer.loadingFailed' defaultMessage='Loading failed.' />
+  </span>
+);
+
+const ResourceRenderer = ({
+  loading = defaultLoading,
+  failed = defaultFailed,
+  children: ready,
+  resource
+}) =>
+  isLoading(resource)
+    ? loading
+    : hasFailed(resource)
+      ? failed
+      : typeof ready === 'function'
+        ? ready(getJsData(resource))
+        : React.cloneElement(ready, getJsData(resource));
+
+ResourceRenderer.propTypes = {
+  loading: PropTypes.element,
+  failed: PropTypes.element,
+  children: PropTypes.oneOfType([ PropTypes.func, PropTypes.element ]).isRequired,
+  resource: PropTypes.object.isRequired
+};
+
+export default ResourceRenderer;
