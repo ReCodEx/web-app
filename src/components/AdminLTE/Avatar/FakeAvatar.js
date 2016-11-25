a20a4f7ceec303bf5715e75d7e6a99684e70ce84
Simon Rozsival <simon@rozsival.com>
2016-11-25 23:37:39 +0100

    avatar refactoring

---
+++ src/components/AdminLTE/Avatar/FakeAvatar.js
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+
+const getSize = (size, small) =>
+  small ? size * (2 / 3) : size;
+
+const FakeAvatar = ({
+  size = 45,
+  borderWidth = 2,
+  light = false,
+  children,
+  small = false
+}) => (
+  <span style={{
+    display: 'inline-block',
+    background: !light ? 'black' : 'white',
+    color: 'gray',
+    textAlign: 'center',
+    width: getSize(size, small),
+    hidth: getSize(size, small),
+    lineHeight: `${getSize(size, small) - 2 * borderWidth}px`,
+    borderStyle: 'solid',
+    borderWidth,
+    borderColor: !light ? 'transparent' : 'gray',
+    borderRadius: Math.ceil(getSize(size, small) / 2),
+    fontSize: Math.floor(Math.min(14, getSize(size, small) / 2))
+  }}>
+    {children}
+  </span>
+);
+
+FakeAvatar.propTypes = {
+  size: PropTypes.number,
+  borderWidth: PropTypes.number,
+  light: PropTypes.bool,
+  children: PropTypes.element.isRequired,
+  small: PropTypes.bool
+};
+
+export default FakeAvatar;
--- src/components/AdminLTE/Avatar/LoadingAvatar.js
+++ src/components/AdminLTE/Avatar/LoadingAvatar.js
@@ -1,33 +1,11 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { LoadingIcon } from '../../Icons';
+import FakeAvatar from './FakeAvatar';
 
-const LoadingAvatar = ({
-  size = 45,
-  borderWidth = 2,
-  light = false
-}) => (
-  <span style={{
-    display: 'inline-block',
-    background: !light ? 'black' : 'white',
-    color: 'gray',
-    textAlign: 'center',
-    width: size,
-    height: size,
-    borderStyle: 'solid',
-    borderWidth,
-    borderColor: !light ? 'transparent' : 'gray',
-    lineHeight: `${size - 2 * borderWidth}px`,
-    borderRadius: Math.ceil(size / 2),
-    fontSize: Math.floor(size / 2)
-  }}>
+const LoadingAvatar = (props) => (
+  <FakeAvatar {...props}>
     <LoadingIcon />
-  </span>
+  </FakeAvatar>
 );
-
-LoadingAvatar.propTypes = {
-  size: PropTypes.number,
-  borderWidth: PropTypes.number,
-  light: PropTypes.bool
-};
 
 export default LoadingAvatar;
