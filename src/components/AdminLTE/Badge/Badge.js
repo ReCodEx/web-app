11c6bf1fe6d228a5482c30da6d6aa791f6da7b97
Šimon Rozsíval <simon@rozsival.com>
2017-04-08 16:26:34 +0200

    badge with links

--- src/components/AdminLTE/Badge/Badge.js
+++ src/components/AdminLTE/Badge/Badge.js
@@ -3,44 +3,53 @@ import { FormattedMessage, FormattedRelative } from 'react-intl';
 import Icon from 'react-fontawesome';
 import { Link } from 'react-router';
 import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+import withLinks from '../../../hoc/withLinks';
 
-const Badge = ({
-  id,
-  fullName,
-  avatarUrl,
-  expiration,
-  logout
-}, {
-  links: {
-    USER_URI_FACTORY,
-    EDIT_USER_URI_FACTORY
+const Badge = (
+  {
+    id,
+    fullName,
+    avatarUrl,
+    expiration,
+    logout,
+    links: {
+      USER_URI_FACTORY,
+      EDIT_USER_URI_FACTORY
+    }
   }
-}) => (
-  <div className='user-panel'>
-    <div className='pull-left image'>
-      <img src={avatarUrl} alt={fullName} className='img-circle' />
+) => (
+  <div className="user-panel">
+    <div className="pull-left image">
+      <img src={avatarUrl} alt={fullName} className="img-circle" />
     </div>
-    <div className='info'>
+    <div className="info">
       <p>
         <Link to={USER_URI_FACTORY(id)}>
           {fullName}
         </Link>
       </p>
       <Link to={EDIT_USER_URI_FACTORY(id)}>
-        <Icon name='edit' />
-        <FormattedMessage id='app.badge.settings' defaultMessage='Settings' />
+        <Icon name="edit" />
+        <FormattedMessage id="app.badge.settings" defaultMessage="Settings" />
       </Link>
       &nbsp;
       <OverlayTrigger
-        placement='right'
-        overlay={(
-          <Tooltip id='tokenExpiration'>
-            <FormattedMessage id='app.badge.sessionExpiration' defaultMessage='Session expiration:' />{' '}
+        placement="right"
+        overlay={
+          <Tooltip id="tokenExpiration">
+            <FormattedMessage
+              id="app.badge.sessionExpiration"
+              defaultMessage="Session expiration:"
+            />
+            {' '}
             <FormattedRelative value={expiration} />
           </Tooltip>
-        )}>
-        <a href='#' onClick={logout}>
-          <Icon name='sign-out' className='text-danger' /> <FormattedMessage id='app.logout' defaultMessage='Logout' />
+        }
+      >
+        <a href="#" onClick={logout}>
+          <Icon name="sign-out" className="text-danger" />
+          {' '}
+          <FormattedMessage id="app.logout" defaultMessage="Logout" />
         </a>
       </OverlayTrigger>
     </div>
@@ -52,11 +61,8 @@ Badge.propTypes = {
   fullName: PropTypes.string.isRequired,
   avatarUrl: PropTypes.string.isRequired,
   expiration: PropTypes.number.isRequired,
-  logout: PropTypes.func
-};
-
-Badge.contextTypes = {
+  logout: PropTypes.func,
   links: PropTypes.object
 };
 
-export default Badge;
+export default withLinks(Badge);
