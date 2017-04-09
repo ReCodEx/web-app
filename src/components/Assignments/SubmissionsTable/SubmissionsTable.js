8097944d81f926d7242a7fe85210c3e4fbc2c3c3
Šimon Rozsíval <simon@rozsival.com>
2017-04-09 13:41:28 +0200

    fix formating

--- src/components/Assignments/SubmissionsTable/SubmissionsTable.js
+++ src/components/Assignments/SubmissionsTable/SubmissionsTable.js
@@ -14,54 +14,92 @@ import FailedLoadingSubmissionTableRow from './FailedLoadingSubmissionTableRow';
 import NotEvaluatedSubmissionTableRow from './NotEvaluatedSubmissionTableRow';
 import EvaluationFailedTableRow from './EvaluationFailedTableRow';
 
-const SubmissionsTable = ({
-  title,
-  assignmentId,
-  submissions
-}, {
-  links: { SUBMISSION_DETAIL_URI_FACTORY }
-}) => (
+const SubmissionsTable = (
+  {
+    title,
+    assignmentId,
+    submissions
+  },
+  {
+    links: { SUBMISSION_DETAIL_URI_FACTORY }
+  }
+) => (
   <Box title={title} collapsable isOpen noPadding>
     <Table responsive>
       <thead>
         <tr>
-          <th></th>
-          <th><FormattedMessage id="app.submissionsTable.submissionDate" defaultMessage="Date of submission" /></th>
-          <th className="text-center"><FormattedMessage id="app.submissionsTable.solutionValidity" defaultMessage="Solution validity" /></th>
-          <th className="text-center"><FormattedMessage id="app.submissionsTable.receivedPoints" defaultMessage="Received points" /></th>
-          <th><FormattedMessage id="app.submissionsTable.note" defaultMessage="Note" /></th>
+          <th />
+          <th>
+            <FormattedMessage
+              id="app.submissionsTable.submissionDate"
+              defaultMessage="Date of submission"
+            />
+          </th>
+          <th className="text-center">
+            <FormattedMessage
+              id="app.submissionsTable.solutionValidity"
+              defaultMessage="Solution validity"
+            />
+          </th>
+          <th className="text-center">
+            <FormattedMessage
+              id="app.submissionsTable.receivedPoints"
+              defaultMessage="Received points"
+            />
+          </th>
+          <th>
+            <FormattedMessage
+              id="app.submissionsTable.note"
+              defaultMessage="Note"
+            />
+          </th>
           <th />
         </tr>
       </thead>
-        <ResourceRenderer
-          resource={submissions.toArray()}
-          loading={<LoadingSubmissionTableRow />}
-          failed={<FailedLoadingSubmissionTableRow />}>
-          {(...submissions) => (
-            <tbody>
-              {submissions.map((data, i) => {
-                const id = data.id;
-                const link = SUBMISSION_DETAIL_URI_FACTORY(assignmentId, id);
+      <ResourceRenderer
+        resource={submissions.toArray()}
+        loading={<LoadingSubmissionTableRow />}
+        failed={<FailedLoadingSubmissionTableRow />}
+      >
+        {(...submissions) => (
+          <tbody>
+            {submissions.map((data, i) => {
+              const id = data.id;
+              const link = SUBMISSION_DETAIL_URI_FACTORY(assignmentId, id);
 
-                switch (data.evaluationStatus) {
-                  case 'done':
-                    return <SuccessfulSubmissionTableRow {...data} key={id} link={link} />;
-                  case 'failed':
-                    return <FailedSubmissionTableRow {...data} key={id} link={link} />;
-                  case 'work-in-progress':
-                    return <NotEvaluatedSubmissionTableRow {...data} key={id} link={link} />;
-                  case 'evaluation-failed':
-                    return <EvaluationFailedTableRow {...data} key={id} link={link} />;
-                  default:
-                    return null;
-                }
-              })}
-            </tbody>
-          )}
-        </ResourceRenderer>
-        {submissions.size === 0 && (
-          <tbody><NoSolutionYetTableRow /></tbody>
+              switch (data.evaluationStatus) {
+                case 'done':
+                  return (
+                    <SuccessfulSubmissionTableRow
+                      {...data}
+                      key={id}
+                      link={link}
+                    />
+                  );
+                case 'failed':
+                  return (
+                    <FailedSubmissionTableRow {...data} key={id} link={link} />
+                  );
+                case 'work-in-progress':
+                  return (
+                    <NotEvaluatedSubmissionTableRow
+                      {...data}
+                      key={id}
+                      link={link}
+                    />
+                  );
+                case 'evaluation-failed':
+                  return (
+                    <EvaluationFailedTableRow {...data} key={id} link={link} />
+                  );
+                default:
+                  return null;
+              }
+            })}
+          </tbody>
         )}
+      </ResourceRenderer>
+      {submissions.size === 0 && <tbody><NoSolutionYetTableRow /></tbody>}
     </Table>
   </Box>
 );
