e29c8a251757060670ae94ee8160c258ee4f9bd6
Petr Stefan <ptr.stef@gmail.com>
2017-04-10 10:12:47 +0200

    Reference solution details

---
+++ src/components/ReferenceSolutions/ReferenceSolutionDetail/ReferenceSolutionDetail.js
@@ -0,0 +1,63 @@
+import React, { PropTypes } from 'react';
+import Icon from 'react-fontawesome';
+import classnames from 'classnames';
+import { FormattedMessage, FormattedNumber, FormattedDate, FormattedTime } from 'react-intl';
+import { Table } from 'react-bootstrap';
+
+import Box from '../../AdminLTE/Box';
+import UsersNameContainer from '../../../containers/UsersNameContainer';
+
+
+const ReferenceSolutionDetail = ({
+  description,
+  uploadedAt,
+  solution: {
+    userId
+  }
+}) => (
+  <Box
+    title={<FormattedMessage id='app.referenceSolutionDetail.title.details' defaultMessage='Reference solution detail' />}
+    noPadding={true}
+    collapsable={true}
+    isOpen={true}>
+    <Table>
+      <tbody>
+        <tr>
+          <td className='text-center'>
+            <Icon name='pencil' />
+          </td>
+          <th><FormattedMessage id='app.referenceSolutionDetail.description' defaultMessage='Description' /></th>
+          <td>{description}</td>
+        </tr>
+        <tr>
+          <td className='text-center'>
+            <Icon name='clock-o' />
+          </td>
+          <th><FormattedMessage id='app.referenceSolutionDetail.uploadedAt' defaultMessage='Uploaded at:' /></th>
+          <td>
+            <FormattedDate value={uploadedAt * 1000} />&nbsp;<FormattedTime value={uploadedAt * 1000} />
+          </td>
+        </tr>
+        <tr>
+          <td className='text-center'>
+            <Icon name='user' />
+          </td>
+          <th><FormattedMessage id='app.referenceSolution.author' defaultMessage='Author:' /></th>
+          <td>
+            <UsersNameContainer userId={userId} />
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  </Box>
+);
+
+ReferenceSolutionDetail.propTypes = {
+  description: PropTypes.string.isRequired,
+  uploadedAt: PropTypes.number.isRequired,
+  solution: PropTypes.shape({
+    userId: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default ReferenceSolutionDetail;
--- src/pages/ReferenceSolution/ReferenceSolution.js
+++ src/pages/ReferenceSolution/ReferenceSolution.js
@@ -2,10 +2,14 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage, defineMessages, intlShape, injectIntl } from 'react-intl';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import { Row, Col, Button } from 'react-bootstrap';
 import Page from '../../components/Page';
 
 import { fetchReferenceSolutionsIfNeeded } from '../../redux/modules/referenceSolutions';
 import { referenceSolutionsSelector } from '../../redux/selectors/referenceSolutions';
+import ReferenceSolutionDetail from '../../components/ReferenceSolutions/ReferenceSolutionDetail';
+import SourceCodeInfoBox from '../../components/SourceCodeInfoBox';
+import SourceCodeViewerContainer from '../../containers/SourceCodeViewerContainer';
 
 
 const messages = defineMessages({
@@ -16,6 +20,10 @@ const messages = defineMessages({
 });
 
 class ReferenceSolution extends Component {
+
+  state = { openFileId: null };
+  openFile = (id) => this.setState({ openFileId: id });
+  hideFile = () => this.setState({ openFileId: null });
 
   static loadAsync = ({ exerciseId }, dispatch) => Promise.all([
     dispatch(fetchReferenceSolutionsIfNeeded(exerciseId))
@@ -37,6 +45,7 @@ class ReferenceSolution extends Component {
       params: { exerciseId, referenceSolutionId },
       intl: { formatMessage }
     } = this.props;
+    const { openFileId } = this.state;
 
     const {
       links: { EXERCISES_URI, EXERCISE_URI_FACTORY }
@@ -65,7 +74,28 @@ class ReferenceSolution extends Component {
         ]}>
           {referenceSolutions => {
             const referenceSolution = referenceSolutions.find(solution => solution.id === referenceSolutionId);
-            return (<div>{referenceSolution.description}</div>)
+            return (
+              <div>
+                <Row>
+                  <Col lg={6}>
+                    <ReferenceSolutionDetail {...referenceSolution} />
+                    <Row>
+                      {referenceSolution.solution.files.map(file => (
+                      <Col lg={6} md={12} key={file.id}>
+                        <a href='#' onClick={() => this.openFile(file.id)}>
+                          <SourceCodeInfoBox {...file} />
+                        </a>
+                      </Col>
+                      ))}
+                    </Row>
+                  </Col>
+                </Row>
+
+                <SourceCodeViewerContainer
+                  show={openFileId !== null}
+                  fileId={openFileId}
+                  onHide={() => this.hideFile()} />
+              </div>)
           }}
       </Page>
     );
