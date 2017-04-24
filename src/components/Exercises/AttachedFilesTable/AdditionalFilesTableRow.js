12500a0426cba94629c655ffbcc4828773901417
simonrozsival <simon@rozsival.com>
2017-04-24 18:09:25 +0200

    correct download link

--- src/components/Exercises/AttachedFilesTable/AdditionalFilesTableRow.js
+++ src/components/Exercises/AttachedFilesTable/AdditionalFilesTableRow.js
@@ -1,18 +1,20 @@
 import React, { PropTypes } from 'react';
 import prettyBytes from 'pretty-bytes';
 import { FormattedDate, FormattedTime } from 'react-intl';
+import withLinks from '../../../hoc/withLinks';
 
 const AdditionalFilesTableRow = (
   {
+    id,
     name,
-    downloadUrl,
     size,
-    uploadedAt
+    uploadedAt,
+    links: { DOWNLOAD }
   }
 ) => (
   <tr>
     <td>{name}</td>
-    <td><a href={downloadUrl} target="_blank">{downloadUrl}</a></td>
+    <td><a href={DOWNLOAD(id)} target="_blank">{DOWNLOAD(id)}</a></td>
     <td>{prettyBytes(size)}</td>
     <td>
       <FormattedDate value={uploadedAt * 1000} />
@@ -23,10 +25,11 @@ const AdditionalFilesTableRow = (
 );
 
 AdditionalFilesTableRow.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  downloadUrl: PropTypes.string.isRequired,
   size: PropTypes.number.isRequired,
-  uploadedAt: PropTypes.number.isRequired
+  uploadedAt: PropTypes.number.isRequired,
+  links: PropTypes.object.isRequired
 };
 
-export default AdditionalFilesTableRow;
+export default withLinks(AdditionalFilesTableRow);
