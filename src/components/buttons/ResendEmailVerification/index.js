363feb324d8da63ac98a6f64b104f8b0dbd76194
simonrozsival <simon@rozsival.com>
2017-05-02 19:56:58 +0200

    reexport statements

---
+++ src/components/buttons/ResendEmailVerification/index.js
@@ -0,0 +1,4 @@
+export default from './ResendEmail';
+export { default as ResendingEmail } from './ResendingEmail';
+export { default as ResendingFailed } from './ResendingFailed';
+export { default as EmailResent } from './EmailResent';
