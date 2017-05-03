eaffa6cd2c2a81d85222e815292bd538ec696e19
simonrozsival <simon@rozsival.com>
2017-05-03 15:16:11 +0200

    add selector

--- src/redux/selectors/submissions.js
+++ src/redux/selectors/submissions.js
@@ -1,8 +1,14 @@
 import { createSelector } from 'reselect';
 
-export const getSubmissions = (state) => state.submissions.get('resources');
-export const getSubmission = (id) =>
+export const getSubmissions = state => state.submissions.get('resources');
+export const getSubmission = id =>
+  createSelector(getSubmissions, submissions => submissions.get(id));
+
+export const isAccepted = id =>
   createSelector(
-    getSubmissions,
-    submissions => submissions.get(id)
+    getSubmission(id),
+    submissions =>
+      (submissions.get('data') === null
+        ? false
+        : submission.getIn(['data', 'accepted']))
   );
