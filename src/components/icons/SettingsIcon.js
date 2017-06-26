43034dd2b689007628b838a2a89b732eec1736b0
Petr Stefan <ptr.stef@gmail.com>
2017-06-26 11:51:10 +0200

    Update SettingsIcon.js
    [ci skip]

--- src/components/icons/SettingsIcon.js
+++ src/components/icons/SettingsIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icon from 'react-fontawesome';
 
-const EditIcon = props => <Icon {...props} name="gear" />;
+const SettingsIcon = props => <Icon {...props} name="gear" />;
 
-export default EditIcon;
+export default SettingsIcon;
