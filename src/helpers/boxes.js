53f2b4924dc29abbaa099b3bd3b17ec366992c4b
Šimon Rozsíval <simon@rozsival.com>
2017-09-10 20:51:01 +0200

    fix pipelines editor wip

--- src/helpers/boxes.js
+++ src/helpers/boxes.js
@@ -1,4 +1,5 @@
-export const isUnknownType = type => type === '?';
+export const UNKNOWN_PORT_TYPE = '?';
+export const isUnknownType = type => type === UNKNOWN_PORT_TYPE;
 export const isArrayType = type => type.indexOf('[]') > 0;
 
 export const getVariablesTypes = (boxTypes, boxes) => {
@@ -26,17 +27,44 @@ export const getVariablesTypes = (boxTypes, boxes) => {
   return variablesTypes;
 };
 
+const transformPort = ({ type, value }, variableTypes) => ({
+  type: isUnknownType(type) ? variableTypes[value].type : type,
+  value
+});
+
+const transformPortTypes = (ports, boxTypePorts, variableTypes) =>
+  Object.keys(boxTypePorts).reduce(
+    (acc, name) => ({
+      ...acc,
+      [name]: ports[name] ? transformPort(ports[name], variableTypes) : null
+    }),
+    {}
+  );
+
 export const transformPipelineDataForApi = (boxTypes, { boxes, variables }) => {
-  console.log(boxTypes);
   const variableTypes = getVariablesTypes(boxTypes, boxes);
   const transformedData = {
     boxes: boxes.map(box => {
-      if (Object.keys(box.portsIn).length === 0) {
+      const boxType = boxTypes.find(boxType => boxType.type === box.type);
+
+      if (Object.keys(boxType.portsIn).length === 0) {
         delete box.portsIn;
+      } else {
+        box.portsIn = transformPortTypes(
+          box.portsIn,
+          boxType.portsIn,
+          variableTypes
+        );
       }
 
-      if (Object.keys(box.portsOut).length === 0) {
+      if (Object.keys(boxType.portsOut).length === 0) {
         delete box.portsOut;
+      } else {
+        box.portsOut = transformPortTypes(
+          box.portsOut,
+          boxType.portsOut,
+          variableTypes
+        );
       }
 
       return box;
@@ -46,7 +74,10 @@ export const transformPipelineDataForApi = (boxTypes, { boxes, variables }) => {
         name: atob(key),
         value: variables[key]
       }))
-      .filter(({ name }) => variableTypes[name])
+      .filter(
+        ({ name }) =>
+          variableTypes[name] && !isUnknownType(variableTypes[name].type)
+      )
       .map(({ name, ...variable }) => ({
         ...variable,
         name,
@@ -55,4 +86,76 @@ export const transformPipelineDataForApi = (boxTypes, { boxes, variables }) => {
   };
 
   return transformedData;
+};
+
+export const createBoxFromFormInputs = (data, boxTypes) => {
+  const boxType = boxTypes.find(box => box.type === data.type);
+  if (!boxType) {
+    throw new Error('No box type was selected.');
+  }
+
+  const allowedPortsIn = Object.keys(boxType.portsIn);
+  const allowedPortsOut = Object.keys(boxType.portsOut);
+
+  const portsIn = data.portsIn
+    ? allowedPortsIn.reduce(
+        (acc, port) =>
+          data.portsIn[port]
+            ? {
+                ...acc,
+                [port]: {
+                  ...data.portsIn[port],
+                  type: boxType.portsIn[port].type
+                }
+              }
+            : acc,
+        {}
+      )
+    : {};
+
+  const portsOut = data.portsOut
+    ? allowedPortsOut.reduce(
+        (acc, port) =>
+          data.portsOut[port]
+            ? {
+                ...acc,
+                [port]: {
+                  ...data.portsOut[port],
+                  type: boxType.portsOut[port].type
+                }
+              }
+            : acc,
+        {}
+      )
+    : {};
+
+  return { ...data, portsIn, portsOut };
+};
+
+const flattenPorts = boxes =>
+  boxes.reduce(
+    (acc, ports) => [
+      ...acc,
+      ...Object.keys(ports)
+        .filter(port => isUnknownType(ports[port].type) === false)
+        .map(port => ({
+          name: port,
+          type: ports[port].type,
+          value: btoa(ports[port].value)
+        }))
+    ],
+    []
+  );
+
+export const extractVariables = (boxes = []) => {
+  const inputs = flattenPorts(
+    boxes.map(box => ({ ...box.portsIn })).filter(ports => ports)
+  );
+
+  // remove duplicities
+  return inputs.reduce(
+    (acc, port) =>
+      !acc.find(item => item.name === port.name) ? [...acc, port] : acc,
+    []
+  );
 };
