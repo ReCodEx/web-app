444fb6d8ad975b75c4268d7b0122f99fdfe36289
Petr Stefan <ptr.stef@gmail.com>
2017-10-12 20:55:38 +0200

    More complex sorting of assignments

--- src/components/Assignments/Assignment/AssignmentsTable/AssignmentsTable.js
+++ src/components/Assignments/Assignment/AssignmentsTable/AssignmentsTable.js
@@ -15,12 +15,41 @@ import AssignmentTableRow, {
   LoadingAssignmentTableRow
 } from '../AssignmentTableRow';
 
+const compareAssignments = (a, b) => {
+  // first compare by deadline
+  if (a.firstDeadline < b.firstDeadline) {
+    return -1;
+  } else if (a.firstDeadline == b.firstDeadline) {
+    // then compare by second deadline - if one of them does not have any,
+    // it is lower -> higher position in the table
+    if (a.allowSecondDeadline != b.allowSecondDeadline) {
+      // one has the second deadline and the other not
+      return a.allowSecondDeadline ? 1 : -1;
+    } else {
+      // if both have second deadline, compare them
+      if (a.allowSecondDeadline == true) {
+        if (a.secondDeadline < b.secondDeadline) {
+          return -1;
+        } else if (a.secondDeadline > b.secondDeadline) {
+          return 1;
+        }
+        // if second deadlines are equal, continue
+      }
+
+      // none of them have second deadline or they are queal, compare creation times
+      return b.createdAt - a.createdAt;
+    }
+  } else {
+    return 1;
+  }
+};
+
 const AssignmentsTable = ({
   assignments = List(),
   statuses = [],
   showGroup = true,
   userId = null
-}) => (
+}) =>
   <Table hover>
     <thead>
       <tr>
@@ -60,8 +89,8 @@ const AssignmentsTable = ({
       {assignments
         .filter(isReady)
         .map(getJsData)
-        .sort((a, b) => a.firstDeadline - b.firstDeadline)
-        .map(assignment => (
+        .sort(compareAssignments)
+        .map(assignment =>
           <AssignmentTableRow
             key={assignment.id}
             item={assignment}
@@ -69,10 +98,9 @@ const AssignmentsTable = ({
             showGroup={showGroup}
             status={statuses[assignment.id]}
           />
-        ))}
+        )}
     </tbody>
-  </Table>
-);
+  </Table>;
 
 AssignmentsTable.propTypes = {
   assignments: ImmutablePropTypes.list.isRequired,
