5eb01b640fd83e2c9edb7f14301a83f18390fac0
Simon Rozsival <simon@rozsival.com>
2017-10-14 18:43:56 +0200

    Add persisting to the store logic

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,16 +4,22 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
+import * as storage from 'redux-storage';
 import authMiddleware from './middleware/authMiddleware';
 import apiMiddleware from './middleware/apiMiddleware';
 import createReducer from './reducer';
+import createEngine from 'redux-storage-engine-localstorage';
+import { actionTypes } from './modules/auth';
+
+const engine = createEngine('recodex/store');
 
 const getMiddleware = history => [
   authMiddleware,
   apiMiddleware,
   promiseMiddleware(),
   thunkMiddleware,
-  routerMiddleware(history)
+  routerMiddleware(history),
+  storage.createMiddleware(engine, [], [actionTypes.LOGIN_SUCCESS])
 ];
 
 const dev = history =>
@@ -26,9 +32,21 @@ const prod = history => compose(applyMiddleware(...getMiddleware(history)));
 
 const isDev = () => process.env.NODE_ENV === 'development';
 
-export const configureStore = (history, initialState, token) =>
-  createStore(
-    createReducer(token),
+export const configureStore = (history, initialState, token) => {
+  const reducer = createReducer(token, (old, next) => ({
+    ...old,
+    userSwitching: next.userSwitching
+  }));
+  const store = createStore(
+    storage.reducer(reducer),
     initialState,
     (isDev() ? dev : prod)(history)
   );
+
+  if (canUseDOM) {
+    const load = storage.createLoader(engine);
+    load(store);
+  }
+
+  return store;
+};
