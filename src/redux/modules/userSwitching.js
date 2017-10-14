84f5b2ee292a539b002e27979c97a8b8adb9cd66
Simon Rozsival <simon@rozsival.com>
2017-10-14 18:43:39 +0200

    Add user switching reducer

---
+++ src/redux/modules/userSwitching.js
@@ -0,0 +1,38 @@
+import { handleActions } from 'redux-actions';
+import { actionTypes } from './auth';
+import { isTokenValid } from '../helpers/token';
+import { addNotification } from './notifications';
+
+export const switchUser = userId => (dispatch, getState) => {
+  const state = getState().userSwitching;
+  const accessToken = state[userId] ? state[userId].accessToken : null;
+  if (!accessToken || !isTokenValid(accessToken)) {
+    dispatch(addNotification({ msg: 'The token has already expired.' }));
+  } else {
+    dispatch({
+      type: actionTypes.LOGIN_SUCCESS,
+      payload: { accessToken },
+      meta: { service: 'takeover' }
+    });
+  }
+};
+
+const initialState = {};
+
+const reducer = handleActions(
+  {
+    [actionTypes.LOGIN_SUCCESS]: (state, { payload: { user, accessToken } }) =>
+      state[user.id]
+        ? state
+        : {
+            ...state,
+            [user.id]: {
+              user: { id: user.id, name: user.name, avatarUrl: user.avatarUrl },
+              accessToken
+            }
+          }
+  },
+  initialState
+);
+
+export default reducer;
