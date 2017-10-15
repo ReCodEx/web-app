42ec1f1ab91d48677ca8f732e55c1a3a559336f2
Petr Stefan <ptr.stef@gmail.com>
2017-10-15 22:17:19 +0200

    Confirm removing students from group

--- src/components/buttons/LeaveGroupButton/LeaveGroupButton.js
+++ src/components/buttons/LeaveGroupButton/LeaveGroupButton.js
@@ -3,17 +3,27 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import Button from '../../widgets/FlatButton';
 import Icon from 'react-fontawesome';
+import Confirm from '../../forms/Confirm';
 
-const LeaveGroupButton = ({ onClick, ...props }) => (
-  <Button {...props} onClick={onClick} bsStyle="warning" className="btn-flat">
-    <Icon name="user-times" />
-    {' '}
-    <FormattedMessage
-      id="app.groups.leaveGroupButton"
-      defaultMessage="Leave group"
-    />
-  </Button>
-);
+const LeaveGroupButton = ({ onClick, ...props }) =>
+  <Confirm
+    id={'confirm'}
+    onConfirmed={onClick}
+    question={
+      <FormattedMessage
+        id="app.leaveGroup.confirm"
+        defaultMessage="Are you sure you want to leave this group?"
+      />
+    }
+  >
+    <Button {...props} onClick={onClick} bsStyle="warning" className="btn-flat">
+      <Icon name="user-times" />{' '}
+      <FormattedMessage
+        id="app.groups.leaveGroupButton"
+        defaultMessage="Leave group"
+      />
+    </Button>
+  </Confirm>;
 
 LeaveGroupButton.propTypes = {
   onClick: PropTypes.func.isRequired
