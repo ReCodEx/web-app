345ba3a9d9a9159e8b8498375e6be746c444d62c
Petr Stefan <ptr.stef@gmail.com>
2017-10-15 21:40:27 +0200

    Allow download supplementary files

--- src/components/Exercises/AttachedFilesTable/SupplementaryFilesTableRow.js
+++ src/components/Exercises/AttachedFilesTable/SupplementaryFilesTableRow.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import prettyBytes from 'pretty-bytes';
 import { FormattedDate, FormattedTime } from 'react-intl';
+import { downloadSupplementaryFile } from '../../../redux/modules/files';
 
-const SupplementaryFilesTableRow = ({ name, hashName, size, uploadedAt }) =>
+const SupplementaryFilesTableRow = ({
+  id,
+  name,
+  hashName,
+  size,
+  uploadedAt,
+  downloadFile
+}) =>
   <tr>
     <td>
-      {name}
+      <a href="#" onClick={downloadFile}>
+        {name}
+      </a>
     </td>
     <td>
       {prettyBytes(size)}
@@ -19,10 +30,20 @@ const SupplementaryFilesTableRow = ({ name, hashName, size, uploadedAt }) =>
   </tr>;
 
 SupplementaryFilesTableRow.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   hashName: PropTypes.string.isRequired,
   size: PropTypes.number.isRequired,
-  uploadedAt: PropTypes.number.isRequired
+  uploadedAt: PropTypes.number.isRequired,
+  downloadFile: PropTypes.func.isRequired
 };
 
-export default SupplementaryFilesTableRow;
+export default connect(
+  (state, props) => ({}),
+  (dispatch, { id }) => ({
+    downloadFile: e => {
+      e.preventDefault();
+      dispatch(downloadSupplementaryFile(id));
+    }
+  })
+)(SupplementaryFilesTableRow);
--- src/redux/modules/files.js
+++ src/redux/modules/files.js
@@ -21,14 +21,14 @@ const actionTypes = {
 export const loadFile = actions.pushResource;
 export const fetchFileIfNeeded = actions.fetchOneIfNeeded;
 
-export const download = id => (dispatch, getState) =>
+const downloadHelper = endpoint => id => (dispatch, getState) =>
   dispatch(fetchFileIfNeeded(id))
     .then(() =>
       dispatch(
         createApiAction({
           type: actionTypes.DOWNLOAD,
           method: 'GET',
-          endpoint: `/uploaded-files/${id}/download`,
+          endpoint: endpoint(id),
           doNotProcess: true // the response is not (does not have to be) a JSON
         })
       )
@@ -40,6 +40,11 @@ export const download = id => (dispatch, getState) =>
       saveAs(typedBlob, file.name);
       return Promise.resolve();
     });
+
+export const download = downloadHelper(id => `/uploaded-files/${id}/download`);
+export const downloadSupplementaryFile = downloadHelper(
+  id => `/uploaded-files/supplementary-file/${id}/download`
+);
 
 /**
  * Reducer
