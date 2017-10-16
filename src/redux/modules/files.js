4fba9f68f52440b39a500a2a9f6c86fc9ae722f0
Simon Rozsival <simon@rozsival.com>
2017-10-16 17:51:14 +0200

    Fix content type

--- src/redux/modules/files.js
+++ src/redux/modules/files.js
@@ -24,15 +24,15 @@ export const download = downloadHelper({
   endpoint: id => `/uploaded-files/${id}/download`,
   fetch: fetchFileIfNeeded,
   actionType: actionTypes.DOWNLOAD,
-  fileNameSelector: (id, state) => `${getJsData(getFile(id)(state)).name}.zip`,
-  contentType: 'application/zip'
+  fileNameSelector: (id, state) => getJsData(getFile(id)(state)).name,
+  contentType: 'text/plain;charset=utf-8'
 });
 export const downloadSupplementaryFile = downloadHelper({
   endpoint: id => `/uploaded-files/supplementary-file/${id}/download`,
   fetch: fetchFileIfNeeded,
   actionType: actionTypes.DOWNLOAD,
-  fileNameSelector: (id, state) => `${getJsData(getFile(id)(state)).name}.zip`,
-  contentType: 'application/zip'
+  fileNameSelector: (id, state) => getJsData(getFile(id)(state)).name,
+  contentType: 'text/plain;charset=utf-8'
 });
 
 /**
