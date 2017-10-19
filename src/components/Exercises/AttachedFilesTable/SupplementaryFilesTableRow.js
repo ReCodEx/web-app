3f447f3eac43e33de9f6bf8a62b64252c04f309a
Petr Stefan <ptr.stef@gmail.com>
2017-10-19 23:33:02 +0200

    Allow removing supplementary files
    May be altered after API implementation, but hopefully it is ok.

--- src/components/Exercises/AttachedFilesTable/SupplementaryFilesTableRow.js
+++ src/components/Exercises/AttachedFilesTable/SupplementaryFilesTableRow.js
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import prettyBytes from 'pretty-bytes';
-import { FormattedDate, FormattedTime } from 'react-intl';
+import { FormattedDate, FormattedTime, FormattedMessage } from 'react-intl';
+import { Button } from 'react-bootstrap';
+import Confirm from '../../../components/forms/Confirm';
+import { DeleteIcon } from '../../../components/icons';
 import { downloadSupplementaryFile } from '../../../redux/modules/files';
+import { removeSupplementaryFile } from '../../../redux/modules/supplementaryFiles';
 
 const SupplementaryFilesTableRow = ({
   id,
@@ -11,7 +15,8 @@ const SupplementaryFilesTableRow = ({
   hashName,
   size,
   uploadedAt,
-  downloadFile
+  downloadFile,
+  removeSupplementaryFile
 }) =>
   <tr>
     <td>
@@ -27,6 +32,27 @@ const SupplementaryFilesTableRow = ({
       &nbsp;
       <FormattedTime value={uploadedAt * 1000} />
     </td>
+    <td>
+      <Confirm
+        id={id}
+        onConfirmed={() => removeSupplementaryFile(id)}
+        question={
+          <FormattedMessage
+            id="app.supplementaryFiles.deleteConfirm"
+            defaultMessage="Are you sure you want to delete the file? This cannot be undone."
+          />
+        }
+        className="pull-right"
+      >
+        <Button bsSize="xs" className="btn-flat" bsStyle="danger">
+          <DeleteIcon />{' '}
+          <FormattedMessage
+            id="app.supplementaryFiles.deleteButton"
+            defaultMessage="Delete"
+          />
+        </Button>
+      </Confirm>
+    </td>
   </tr>;
 
 SupplementaryFilesTableRow.propTypes = {
@@ -35,7 +61,8 @@ SupplementaryFilesTableRow.propTypes = {
   hashName: PropTypes.string.isRequired,
   size: PropTypes.number.isRequired,
   uploadedAt: PropTypes.number.isRequired,
-  downloadFile: PropTypes.func.isRequired
+  downloadFile: PropTypes.func.isRequired,
+  removeSupplementaryFile: PropTypes.func.isRequired
 };
 
 export default connect(
@@ -44,6 +71,7 @@ export default connect(
     downloadFile: e => {
       e.preventDefault();
       dispatch(downloadSupplementaryFile(id));
-    }
+    },
+    removeSupplementaryFile: fileId => dispatch(removeSupplementaryFile(fileId))
   })
 )(SupplementaryFilesTableRow);
