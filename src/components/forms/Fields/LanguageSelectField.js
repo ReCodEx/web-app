66b51303a509647e761bc7b71c0f8273a19f2e2f
Martin Krulis <krulis@ksi.mff.cuni.cz>
2017-10-29 01:02:47 +0200

    German language removed from list of offered localizations.

--- src/components/forms/Fields/LanguageSelectField.js
+++ src/components/forms/Fields/LanguageSelectField.js
@@ -2,18 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SelectField from './SelectField';
 
-const LanguageSelectField = ({ options = [], ...props }) => (
+const LanguageSelectField = ({ options = [], ...props }) =>
   <SelectField
     {...props}
     options={[
       { key: '', name: '...' },
       { key: 'en', name: 'English' },
       { key: 'cs', name: 'Čeština' },
-      { key: 'de', name: 'Deutsch' },
       ...options
     ]}
-  />
-);
+  />;
 
 LanguageSelectField.propTypes = {
   options: PropTypes.array
