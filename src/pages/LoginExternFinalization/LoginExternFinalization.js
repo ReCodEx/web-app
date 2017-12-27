cac24e33ddff6f1d2f3cf4c1b3fbda2f16a83665
Martin Krulis <krulis@ksi.mff.cuni.cz>
2017-12-27 14:06:09 +0100

    Attempt to fix a minor problem with CAS auth.

--- src/pages/LoginExternFinalization/LoginExternFinalization.js
+++ src/pages/LoginExternFinalization/LoginExternFinalization.js
@@ -14,14 +14,23 @@ import styles from './LoginExternFinalization.less';
  * Also controls the state of the sidebar - collapsing and showing the sidebar.
  */
 class LoginExternFinalization extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.closeTimeout = null;
+  }
+
   messageHandler = e => {
     if (e.data === 'received') {
       // Once the message with ticket was received, our seconds are numbered...
-      window.setTimeout(() => {
+      this.closeTimeout = window.setTimeout(() => {
         window.close();
       }, 2000);
     } else if (e.data === 'die') {
       // An advice to drop dead immediately ...
+      if (this.closeTimeout !== null) {
+        window.clearTimeout(this.closeTimeout);
+        this.closeTimeout = null;
+      }
       window.close();
     }
   };
