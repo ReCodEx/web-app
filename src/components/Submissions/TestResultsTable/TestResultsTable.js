bb1cc9aa97e47db94b02bd74668c5bcc440f5df2
Martin Krulis <krulis@ksi.mff.cuni.cz>
2018-01-23 23:46:04 +0100

    Make results table show cpu or wall time based on which one was set.

--- src/components/Submissions/TestResultsTable/TestResultsTable.js
+++ src/components/Submissions/TestResultsTable/TestResultsTable.js
@@ -12,7 +12,7 @@ import exitCodeMapping from '../../helpers/exitCodeMapping';
 const hasValue = value => value !== null;
 
 const tickOrCrossAndRatioOrValue = (isOK, ratio, value, pretty, multiplier) =>
-  <td
+  <span
     className={classNames({
       'text-center': true,
       'text-success': isOK,
@@ -32,7 +32,81 @@ const tickOrCrossAndRatioOrValue = (isOK, ratio, value, pretty, multiplier) =>
       {hasValue(value) && ') '}
       {hasValue(value) && pretty(value * multiplier)}
     </small>
-  </td>;
+  </span>;
+
+const showTimeResults = (
+  key,
+  wallTime,
+  wallTimeRatio,
+  wallTimeExceeded,
+  cpuTime,
+  cpuTimeRatio,
+  cpuTimeExceeded
+) => {
+  const showWall =
+    Boolean(wallTimeRatio) || (wallTimeExceeded && !cpuTimeExceeded);
+  const showCpu = Boolean(cpuTimeRatio) || cpuTimeExceeded || !showWall;
+  return (
+    <table style={{ display: 'inline-block' }}>
+      <tbody>
+        {showCpu &&
+          <tr>
+            <td className="text-muted">
+              <OverlayTrigger
+                placement="top"
+                overlay={
+                  <Tooltip id="wall-time-icon">
+                    <FormattedMessage
+                      id="app.submissions.testResultsTable.cpuTimeExplain"
+                      defaultMessage="CPU time (total time the CPU was used by all threads)"
+                    />
+                  </Tooltip>
+                }
+              >
+                <Icon name="microchip" style={{ marginRight: '10px' }} />
+              </OverlayTrigger>
+            </td>
+            <td className="text-left">
+              {tickOrCrossAndRatioOrValue(
+                cpuTimeExceeded === false,
+                cpuTimeRatio,
+                cpuTime,
+                prettyMs,
+                1000
+              )}
+            </td>
+          </tr>}
+        {showWall &&
+          <tr>
+            <td className="text-muted">
+              <OverlayTrigger
+                placement="top"
+                overlay={
+                  <Tooltip id="wall-time-icon">
+                    <FormattedMessage
+                      id="app.submissions.testResultsTable.wallTimeExplain"
+                      defaultMessage="Wall time (real-time measured by external clock)"
+                    />
+                  </Tooltip>
+                }
+              >
+                <Icon name="clock-o" style={{ marginRight: '10px' }} />
+              </OverlayTrigger>
+            </td>
+            <td className="text-left">
+              {tickOrCrossAndRatioOrValue(
+                wallTimeExceeded === false,
+                wallTimeRatio,
+                wallTime,
+                prettyMs,
+                1000
+              )}
+            </td>
+          </tr>}
+      </tbody>
+    </table>
+  );
+};
 
 const TestResultsTable = ({ results, runtimeEnvironmentId }) =>
   <Table responsive>
@@ -85,7 +159,7 @@ const TestResultsTable = ({ results, runtimeEnvironmentId }) =>
           >
             <span>
               <Icon name="thermometer-half" />&nbsp;&nbsp;
-              <Icon name="microchip" />
+              <Icon name="braille" />
             </span>
           </OverlayTrigger>
         </th>
@@ -133,11 +207,14 @@ const TestResultsTable = ({ results, runtimeEnvironmentId }) =>
           status,
           memoryExceeded,
           wallTimeExceeded,
+          cpuTimeExceeded,
           message,
-          wallTimeRatio,
           memoryRatio,
-          wallTime,
+          wallTimeRatio,
+          cpuTimeRatio,
           memory,
+          wallTime,
+          cpuTime,
           exitCode
         }) =>
           <tr key={testName}>
@@ -181,20 +258,26 @@ const TestResultsTable = ({ results, runtimeEnvironmentId }) =>
               </b>
             </td>
 
-            {tickOrCrossAndRatioOrValue(
-              memoryExceeded === false,
-              memoryRatio,
-              memory,
-              prettyPrintBytes,
-              1024
-            )}
-            {tickOrCrossAndRatioOrValue(
-              wallTimeExceeded === false,
-              wallTimeRatio,
-              wallTime,
-              prettyMs,
-              1000
-            )}
+            <td className="text-center">
+              {tickOrCrossAndRatioOrValue(
+                memoryExceeded === false,
+                memoryRatio,
+                memory,
+                prettyPrintBytes,
+                1024
+              )}
+            </td>
+            <td className="text-center">
+              {showTimeResults(
+                testName,
+                wallTime,
+                wallTimeRatio,
+                wallTimeExceeded,
+                cpuTime,
+                cpuTimeRatio,
+                cpuTimeExceeded
+              )}
+            </td>
 
             <td className="text-center">
               {exitCodeMapping(runtimeEnvironmentId, exitCode)}
