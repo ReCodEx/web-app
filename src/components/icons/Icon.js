04a990bd09bfc27dece1b267a4de66f7ee866afd
Martin Kruliš <krulis@ksi.mff.cuni.cz>
2018-04-21 19:55:16 +0200

    Create universal SortableTable widget and using it to make Results ta… (#218)
    * Create universal SortableTable widget and using it to make Results table on Group Detail page sortable.
    
    * Fixing icon for collapsing sidebar.
    
    * Adjusting behavior of sorting controls.

--- src/components/icons/Icon.js
+++ src/components/icons/Icon.js
@@ -5,9 +5,16 @@ import classNames from 'classnames';
 import style from './Icon.less';
 
 const Icon = ({
+  smallGapLeft = false,
+  smallGapRight = false,
   gapLeft = false,
   gapRight = false,
+  largeGapLeft = false,
+  largeGapRight = false,
+  timid = false,
   className = [],
+  onClick,
+  onDoubleClick,
   ...props
 }) => {
   const passedClassNames =
@@ -17,17 +24,32 @@ const Icon = ({
       {...props}
       className={classNames({
         ...passedClassNames,
+        [style.smallGapLeft]: smallGapLeft,
+        [style.smallGapRight]: smallGapRight,
         [style.gapLeft]: gapLeft,
-        [style.gapRight]: gapRight
+        [style.gapRight]: gapRight,
+        [style.largeGapLeft]: largeGapLeft,
+        [style.largeGapRight]: largeGapRight,
+        [style.timid]: timid,
+        [style.clickable]: Boolean(onClick || onDoubleClick)
       })}
+      onClick={onClick}
+      onDoubleClick={onDoubleClick}
     />
   );
 };
 
 Icon.propTypes = {
   className: PropTypes.any,
+  smallGapLeft: PropTypes.bool,
+  smallGapRight: PropTypes.bool,
   gapLeft: PropTypes.bool,
-  gapRight: PropTypes.bool
+  gapRight: PropTypes.bool,
+  largeGapLeft: PropTypes.bool,
+  largeGapRight: PropTypes.bool,
+  timid: PropTypes.bool,
+  onClick: PropTypes.func,
+  onDoubleClick: PropTypes.func
 };
 
 export default Icon;
