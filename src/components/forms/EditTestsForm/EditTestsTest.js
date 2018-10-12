e0ae1708a197ebdd2c91907330856a065a76bdb9
Martin Krulis <krulis@ksi.mff.cuni.cz>
2018-10-12 23:28:35 +0200

    Improving behavior of Tests form on the Exercise Config Page.

--- src/components/forms/EditTestsForm/EditTestsTest.js
+++ src/components/forms/EditTestsForm/EditTestsTest.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Table, Button } from 'react-bootstrap';
+import { Table, Alert } from 'react-bootstrap';
 import { formValues } from 'redux-form';
 import { FormattedMessage } from 'react-intl';
+
 import EditTestsTestRow from './EditTestsTestRow';
+import Button from '../../widgets/FlatButton';
 import { prettyPrintPercent } from '../../helpers/stringFormatters';
 import { AddIcon } from '../../icons';
 
@@ -14,70 +16,58 @@ const EditTestsTest = ({ fields, isUniform, testValues, readOnly = false }) => {
 
   return (
     <div>
-      <Table>
-        <thead>
-          <tr>
-            <th>
-              <FormattedMessage
-                id="app.editTestsTest.name"
-                defaultMessage="Test name:"
-              />
-            </th>
-            {!isUniform &&
-              <th>
-                <FormattedMessage
-                  id="app.editTestsTest.weight"
-                  defaultMessage="Test weight:"
+      {fields.length > 0
+        ? <Table>
+            <thead>
+              <tr>
+                <th>
+                  <FormattedMessage
+                    id="app.editTestsTest.name"
+                    defaultMessage="Test name:"
+                  />
+                </th>
+                {!isUniform &&
+                  <th>
+                    <FormattedMessage
+                      id="app.editTestsTest.weight"
+                      defaultMessage="Test weight:"
+                    />
+                  </th>}
+                <th>
+                  <FormattedMessage
+                    id="app.editTestsTest.pointsPercentage"
+                    defaultMessage="Points Percentage:"
+                  />
+                </th>
+                {!readOnly && <th />}
+              </tr>
+            </thead>
+            <tbody>
+              {fields.map((test, index) =>
+                <EditTestsTestRow
+                  key={index}
+                  test={test}
+                  isUniform={isUniform}
+                  readOnly={readOnly}
+                  percent={
+                    testValues[index] && testValues[index].weight
+                      ? prettyPrintPercent(
+                          (isUniform ? 1 : Number(testValues[index].weight)) /
+                            weightSum
+                        )
+                      : '?'
+                  }
+                  onRemove={() => fields.remove(index)}
                 />
-              </th>}
-            <th>
-              <FormattedMessage
-                id="app.editTestsTest.pointsPercentage"
-                defaultMessage="Points Percentage:"
-              />
-            </th>
-            {!readOnly && <th />}
-          </tr>
-        </thead>
-        <tbody>
-          {fields.map((test, index) =>
-            <EditTestsTestRow
-              key={index}
-              test={test}
-              isUniform={isUniform}
-              readOnly={readOnly}
-              percent={
-                testValues[index] && testValues[index].weight
-                  ? prettyPrintPercent(
-                      (isUniform ? 1 : Number(testValues[index].weight)) /
-                        weightSum
-                    )
-                  : '?'
-              }
-              onRemove={() => fields.remove(index)}
-            />
-          )}
-        </tbody>
-      </Table>
-      {fields.length < 99 &&
-        !readOnly &&
-        <div style={{ textAlign: 'right' }}>
-          <Button
-            onClick={() =>
-              fields.push({
-                name: 'Test ' + (fields.length + 1).toString().padStart(2, '0'),
-                weight: '100'
-              })}
-            bsStyle={'primary'}
-            className="btn-flat"
-          >
-            <AddIcon gapRight />
+              )}
+            </tbody>
+          </Table>
+        : <Alert bsStyle="warning">
             <FormattedMessage
-              id="app.editTestsTest.add"
-              defaultMessage="Add Test"
+              id="app.editTestsTest.noTests"
+              defaultMessage="There are no tests yet. It is highly recommended to set up the tests first since most of the remaining configurations depends on them."
             />
-          </Button>
-        </div>}
+          </Alert>}
     </div>
   );
 };
