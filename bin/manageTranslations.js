ea68db42ab3903bdc6c4ecf4a1e9b11fdeb81f33
Martin Krulis <krulis@ksi.mff.cuni.cz>
2018-10-13 21:19:43 +0200

    Fixing translation process, adding white lists.

--- bin/manageTranslations.js
+++ bin/manageTranslations.js
@@ -1,7 +1,15 @@
 import manageTranslations from 'react-intl-translations-manager';
+import fs from 'fs'
+
+const translationsDirectory = './src/locales/';
 
 manageTranslations({
   messagesDirectory: './tmp/messages',
-  translationsDirectory: './src/locales/',
-  languages: ['en', 'cs']
+  translationsDirectory,
+  languages: ['en', 'cs'],
+  detectDuplicateIds: false
 });
+
+const enData = JSON.parse(fs.readFileSync(translationsDirectory + 'en.json', 'utf8'));
+const whiteList = Object.keys(enData);
+fs.writeFileSync(translationsDirectory + 'whitelist_en.json', JSON.stringify(whiteList, null, 2));
