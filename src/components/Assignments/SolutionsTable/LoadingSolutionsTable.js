dece3fb2ef1588c33dc45912dfacdc5d5e3d4427
Martin Polanka <PolankaMartin@gmail.com>
2019-01-02 12:28:39 +0100

    Refactor loading solutions table components

---
+++ src/components/Assignments/SolutionsTable/LoadingSolutionsTable.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { LoadingIcon } from '../../icons';
+
+const LoadingSolutionsTable = () =>
+  <div className="text-center em-padding">
+    <LoadingIcon gapRight />
+    <FormattedMessage
+      id="app.solutionsTable.loading"
+      defaultMessage="Loading submitted solutions..."
+    />
+  </div>;
+
+export default LoadingSolutionsTable;
