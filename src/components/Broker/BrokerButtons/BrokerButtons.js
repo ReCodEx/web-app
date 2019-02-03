d339b18f0d146269bb6e566545ff845a4f207f55
Martin Polanka <PolankaMartin@gmail.com>
2019-02-03 12:01:48 +0100

    Refactor brokerStats redux module to general broker resource. Add freeze and unfreeze commands

--- src/components/Broker/BrokerButtons/BrokerButtons.js
+++ src/components/Broker/BrokerButtons/BrokerButtons.js
@@ -2,36 +2,60 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { ButtonGroup } from 'react-bootstrap';
+import Confirm from '../../forms/Confirm';
 
 import Button from '../../widgets/FlatButton';
 import Icon, { RefreshIcon } from '../../icons';
 
-const BrokerButtons = ({ refreshBrokerStats }) =>
+const BrokerButtons = ({ refreshBrokerStats, freezeBroker, unfreezeBroker }) =>
   <div className="em-margin-bottom em-margin-right">
     <ButtonGroup>
       <Button onClick={refreshBrokerStats} bsStyle="primary">
         <RefreshIcon gapRight />
         <FormattedMessage id="app.broker.refresh" defaultMessage="Refresh" />
       </Button>
-      {/* <Button bsStyle="danger">
-    <Icon icon="times" gapRight />
-    <FormattedMessage
-      id="app.broker.freeze"
-      defaultMessage="Freeze"
-    />
-  </Button>
-  <Button bsStyle="success">
-    <Icon icon="sun" gapRight />
-    <FormattedMessage
-      id="app.broker.unfreeze"
-      defaultMessage="Unfreeze"
-    />
-  </Button> */}
+
+      <Confirm
+        id={'freeze-broker'}
+        onConfirmed={() => freezeBroker()}
+        question={
+          <FormattedMessage
+            id="app.broker.confirmFreeze"
+            defaultMessage="Are you sure you want to freeze broker? Broker will not longer accept evaluation requests!"
+          />
+        }
+      >
+        <Button bsStyle="danger">
+          <Icon icon="times" gapRight />
+          <FormattedMessage id="app.broker.freeze" defaultMessage="Freeze" />
+        </Button>
+      </Confirm>
+
+      <Confirm
+        id={'unfreeze-broker'}
+        onConfirmed={() => unfreezeBroker()}
+        question={
+          <FormattedMessage
+            id="app.broker.confirmUnfreeze"
+            defaultMessage="Are you sure you want to unfreeze broker?"
+          />
+        }
+      >
+        <Button bsStyle="success">
+          <Icon icon="sun" gapRight />
+          <FormattedMessage
+            id="app.broker.unfreeze"
+            defaultMessage="Unfreeze"
+          />
+        </Button>
+      </Confirm>
     </ButtonGroup>
   </div>;
 
 BrokerButtons.propTypes = {
-  refreshBrokerStats: PropTypes.func.isRequired
+  refreshBrokerStats: PropTypes.func.isRequired,
+  freezeBroker: PropTypes.func.isRequired,
+  unfreezeBroker: PropTypes.func.isRequired
 };
 
 export default BrokerButtons;
--- src/redux/selectors/brokerStats.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-
-const getBrokerStats = state => state.brokerStats;
-
-export const brokerStatsSelector = createSelector(getBrokerStats, stats =>
-  stats.get('resources')
-);
