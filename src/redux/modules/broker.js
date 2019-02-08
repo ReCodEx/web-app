55e0501f650e258781919c2ac829606986189791
Martin Polanka <PolankaMartin@gmail.com>
2019-02-08 18:20:48 +0100

    Broker redux state should now contain even freeze and unfreeze actions and their updates

--- src/redux/modules/broker.js
+++ src/redux/modules/broker.js
@@ -9,7 +9,13 @@ export const actionTypes = {
   BROKER_STATS_FETCH_FULFILLED: 'recodex/broker/STATS_FETCH_FULFILLED',
   BROKER_STATS_FETCH_REJECTED: 'recodex/broker/STATS_FETCH_REJECTED',
   BROKER_FREEZE: 'recodex/broker/FREEZE',
-  BROKER_UNFREEZE: 'recodex/broker/UNFREEZE'
+  BROKER_FREEZE_PENDING: 'recodex/broker/FREEZE_PENDING',
+  BROKER_FREEZE_FULFILLED: 'recodex/broker/FREEZE_FULFILLED',
+  BROKER_FREEZE_REJECTED: 'recodex/broker/FREEZE_REJECTED',
+  BROKER_UNFREEZE: 'recodex/broker/UNFREEZE',
+  BROKER_UNFREEZE_PENDING: 'recodex/broker/UNFREEZE_PENDING',
+  BROKER_UNFREEZE_FULFILLED: 'recodex/broker/UNFREEZE_FULFILLED',
+  BROKER_UNFREEZE_REJECTED: 'recodex/broker/UNFREEZE_REJECTED'
 };
 
 /**
@@ -57,6 +63,32 @@ const reducer = handleActions(
       [actionTypes.BROKER_STATS_FETCH_REJECTED]: state =>
         state.setIn(
           ['resources', 'stats'],
+          createRecord({ state: resourceStatus.FAILED })
+        ),
+
+      [actionTypes.BROKER_FREEZE_PENDING]: state =>
+        state.setIn(['resources', 'freeze'], createRecord()),
+      [actionTypes.BROKER_FREEZE_FULFILLED]: state =>
+        state.setIn(
+          ['resources', 'freeze'],
+          createRecord({ state: resourceStatus.FULFILLED })
+        ),
+      [actionTypes.BROKER_FREEZE_REJECTED]: state =>
+        state.setIn(
+          ['resources', 'freeze'],
+          createRecord({ state: resourceStatus.FAILED })
+        ),
+
+      [actionTypes.BROKER_UNFREEZE_PENDING]: state =>
+        state.setIn(['resources', 'unfreeze'], createRecord()),
+      [actionTypes.BROKER_UNFREEZE_FULFILLED]: state =>
+        state.setIn(
+          ['resources', 'unfreeze'],
+          createRecord({ state: resourceStatus.FULFILLED })
+        ),
+      [actionTypes.BROKER_UNFREEZE_REJECTED]: state =>
+        state.setIn(
+          ['resources', 'unfreeze'],
           createRecord({ state: resourceStatus.FAILED })
         )
     }
