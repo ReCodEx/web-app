ceab103217961423443e50939404294e78d554ee
Martin Krulis <krulis@ksi.mff.cuni.cz>
2019-12-06 02:05:51 +0100

    Code syntax highlighting added to markdown fields.

--- src/components/widgets/Markdown/Markdown.js
+++ src/components/widgets/Markdown/Markdown.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const md = require('markdown-it')().use(require('@iktakahiro/markdown-it-katex'));
+const hljs = require('highlight.js');
+const md = require('markdown-it')({
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (error) {
+        /* just ignore the error */
+      }
+    }
+
+    return ''; // use external default escaping
+  },
+}).use(require('@iktakahiro/markdown-it-katex'));
 
 const Markdown = ({ source }) => (
   <div
