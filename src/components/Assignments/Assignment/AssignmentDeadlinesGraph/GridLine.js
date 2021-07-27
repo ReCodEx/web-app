3aee1fc38c9f26eb2a50a68cbec73d818071bf4a
Martin Krulis <krulis@d3s.mff.cuni.cz>
2021-07-27 02:23:09 +0200

    Implementing graph to visualize relationship between points limit and deadlines of an assignment.

---
+++ src/components/Assignments/Assignment/AssignmentDeadlinesGraph/GridLine.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GridLine = ({ x = null, y = null, width, height, isMarker = false, strokeWidth = isMarker ? 0.3 : 0.1 }) => {
+  const strokeGradient = x === null ? 'gridlineHorizontal' : 'gridlineVertical';
+  const boundingBoxCoordinates = { x1: 0, y1: -10, x2: width, y2: height + 10 };
+  // the +- 0.001 is actually a hack to create non-zero bounding box, which is necessary for the gradients to work
+  const lineCoordinates =
+    x === null
+      ? { y1: (1 - y) * height - 0.001, y2: (1 - y) * height + 0.001 } // y is automatically flipped (100% is at the top, 0% is at bottom)
+      : { x1: x * width - 0.001, x2: x * width + 0.001 };
+  return (
+    <line
+      {...boundingBoxCoordinates}
+      {...lineCoordinates}
+      stroke={`url(#${strokeGradient})`}
+      strokeWidth={strokeWidth}
+      strokeDasharray="1"
+      strokeLinecap="round"
+    />
+  );
+};
+
+GridLine.propTypes = {
+  x: PropTypes.number,
+  y: PropTypes.number,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  strokeWidth: PropTypes.number,
+  isMarker: PropTypes.bool,
+};
+
+export default GridLine;
