6520139dc09129e075f7a3bfd800854d4b5abd81
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2024-12-08 23:20:56 +0100

    Fixing bugs and adding x-mas appearance modification.

--- src/components/widgets/Sidebar/UserPanel/UserPanelLoading.js
+++ src/components/widgets/Sidebar/UserPanel/UserPanelLoading.js
@@ -3,16 +3,12 @@ import { FormattedMessage } from 'react-intl';
 import { LoadingAvatar } from '../../Avatar';
 
 const UserPanelLoading = props => (
-  <>
-    <div className="float-start me-3">
-      <LoadingAvatar {...props} />
-    </div>
-    <div className="text-light pt-2 sidebar-up-hide-collapsed">
-      <p>
-        <FormattedMessage id="generic.loading" defaultMessage="Loading..." />
-      </p>
-    </div>
-  </>
+  <div className="text-center">
+    <LoadingAvatar {...props} />
+    <span className="text-light ms-3 sidebar-up-hide-collapsed">
+      <FormattedMessage id="generic.loading" defaultMessage="Loading..." />
+    </span>
+  </div>
 );
 
 export default UserPanelLoading;
