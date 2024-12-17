66221cb80170fc2af59b4286e921d87d94573579
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2024-12-17 16:25:48 +0100

    Fixing appearance for narrow screens and sidebar handling.

--- src/redux/selectors/sidebar.js
+++
@@ -1,3 +0,0 @@
-export const getSidebar = state => state.sidebar;
-export const isVisible = state => getSidebar(state).get('visible');
-export const isCollapsed = state => getSidebar(state).get('collapsed');
